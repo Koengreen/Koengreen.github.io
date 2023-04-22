fbedce4e988eb177e668fba34323ee8f0d394d01
Koengreen <302261150@student.rocmondriaan.nl>
2023-04-22 21:08:08 +0200

    Foto's toegevoegd

--- js/script.js
+++ js/script.js
@@ -12,4 +12,24 @@ window.addEventListener("scroll", function () {
       }
     }
   });
+  // Get all links with the "smooth-scroll" class
+const smoothScrollLinks = document.querySelectorAll('.smooth-scroll');
+
+// Add click event listener to each link
+smoothScrollLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    // Prevent default link behavior
+    e.preventDefault();
+
+    // Get the target element's ID from the href attribute
+    const targetId = link.getAttribute('href');
+
+    // Get the target element
+    const targetElement = document.querySelector(targetId);
+
+    // Scroll to the target element smoothly
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
   

3ebb7553bc4ab8407abc24f2f734dd00ef8c946b
Koengreen <302261150@student.rocmondriaan.nl>
2023-04-22 06:49:34 +0200

    Animatie toegevoegd tabel

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-s
+window.addEventListener("scroll", function () {
+    const progressBars = document.querySelectorAll(".progress-bar");
+    const windowHeight = window.innerHeight;
+  
+    for (let progressBar of progressBars) {
+      const progressBarPosition = progressBar.getBoundingClientRect().top;
+      const progressBarPercentage = progressBar.getAttribute("data-percentage");
+  
+      if (progressBarPosition < windowHeight) {
+        progressBar.style.width = progressBarPercentage + "%";
+        progressBar.classList.add("progress-bar-fill");
+      }
+    }
+  });
+  
